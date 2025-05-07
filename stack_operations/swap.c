399f3eca147920354287d0d5089fd067eee2d63a
Rafik Babayan <rababaya@c2r11s4.42yerevan.am>
2025-05-07 13:54:51 +0400

    all over

--- stack_operations/swap.c
+++ stack_operations/swap.c
@@ -6,7 +6,7 @@
 /*   By: rababaya <rababaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 14:27:22 by rababaya          #+#    #+#             */
-/*   Updated: 2025/04/21 14:59:32 by rababaya         ###   ########.fr       */
+/*   Updated: 2025/05/07 13:53:34 by rababaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	swap(t_stack **stack, char c)
 {
 	t_stack	*tmp;
 
+	if (!*stack || !(*stack)->next)
+		return ;
 	tmp = (*stack)->next->next;
 	(*stack)->next->next = *stack;
 	(*stack) = (*stack)->next;
